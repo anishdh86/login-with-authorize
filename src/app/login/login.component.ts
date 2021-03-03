f66c6b32df1b6d5e0c43c157fad8b29ec3e97a23
Anish Dhingra <webdesigner.anish86@gmail.com>
2021-03-03 22:18:56 +0100

    -  Added login method -  Added handle error logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
-import { AuthService } from '../services/auth.service';
+import { AuthResponse, AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -13,22 +14,31 @@ export class LoginComponent implements OnInit {
   isNewUser = true;
   
   constructor(private authService: AuthService) { }
-
+  
   ngOnInit(): void {
   }
-
+  
   switchMode(): void {
     this.isNewUser = !this.isNewUser;
   }
-
+  
   saveFormData(form: NgForm) {
     if(form.valid) {
+      let userObs$: Observable<AuthResponse>;
       const email = form.value.email;
       const password = form.value.password;
 
-      this.authService.register(email, password).subscribe(
+      if(this.isNewUser) {
+        userObs$ = this.authService.register(email, password);
+      } else {
+        userObs$ = this.authService.login(email, password);
+      }
+
+      userObs$.subscribe(
         response => console.log(response),
-        error => this.errorMsg = error.error.error.message
+        errorMessage => {
+          this.errorMsg = errorMessage;
+        }
       );
       form.reset();
     }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient} from '@angular/common/http';
-import { Observable } from "rxjs";
+import { HttpClient, HttpErrorResponse} from '@angular/common/http';
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
-interface AuthResponse {
+export interface AuthResponse {
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({
@@ -15,12 +17,17 @@ interface AuthResponse {
 })
 export class AuthService {
   
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
-  login() {
-
+  login(email: string, password: string): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDu_oPdGdkd2n6OHhBX_RRzh5G4AbXkwOw',
+      {
+        email,
+        password,
+        returnSecureToken: true
+      } 
+    ).pipe(catchError(this.handleError))
   }
 
   register(email: string, password: string): Observable<AuthResponse> {
@@ -31,6 +38,24 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    )
+    ).pipe(catchError(this.handleError))
+  }
+
+  private handleError(errorResponse: HttpErrorResponse) {
+    let errorMsg = 'An error occured!'
+        if (!errorResponse.error || !errorResponse.error.error) {
+          return throwError(errorMsg);
+        }
+        switch(errorResponse.error.error.message) {
+          case 'EMAIL_EXISTS': 
+            errorMsg = 'This email is already in use. Please choose another email.';
+          break;
+          case 'EMAIL_NOT_FOUND': 
+            errorMsg = 'No user could be found with the email you entered.';
+          break;
+          case 'INVALID_PASSWORD':
+            errorMsg = 'The password you entered is incorrect.';
+        }
+        return throwError(errorMsg);
   }
 }
